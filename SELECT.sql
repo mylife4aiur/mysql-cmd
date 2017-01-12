b5a68340229149d365f1daceb265693e8359263c
Jason Yang <hao_yu_yang@outlook.com>
2017-01-12 19:14:55 +0800

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -4,4 +4,5 @@ SELECT count(*) FROM databases.table where FID0001='1' and FID0002 <> '0';
 -- the SELECT operation can't creat a new table
 CREATE TABLE CID_to_CPI1 SELECT `cpi1/dbid`.*,newfinaldrugnamecid.CID FROM `cpi1/dbid;
 -- Left join
-CREATE TABLE CID_to_CPI1 SELECT `cpi1/dbid`.*,newfinaldrugnamecid.CID FROM `cpi1/dbid` LEFT JOIN newfinaldrugnamecid  ON `cpi1/dbid`.DB_ID = newfinaldrugnamecid.DB_ID;
+CREATE TABLE CID_to_CPI1 SELECT `cpi1/dbid`.*,newfinaldrugnamecid.CID FROM `cpi1/dbid` 
+LEFT JOIN newfinaldrugnamecid  ON `cpi1/dbid`.DB_ID = newfinaldrugnamecid.DB_ID;
