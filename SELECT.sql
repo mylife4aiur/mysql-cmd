0fc3a8325c39354cd1fd4db52a585fac125d166f
Jason Yang <hao_yu_yang@outlook.com>
2017-01-12 19:11:02 +0800

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -5,6 +5,7 @@ SELECT count(*) FROM databases.table where FID0001='1' and FID0002 <> '0';
 CREATE TABLE CID_to_CPI1
 SELECT `cpi1/dbid`.*,newfinaldrugnamecid.CID
 FROM `cpi1/dbid
+
 # Left join
 CREATE TABLE CID_to_CPI1
 SELECT `cpi1/dbid`.*,newfinaldrugnamecid.CID
