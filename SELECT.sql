c544efde1a8cd8828844afcecc5b9cc8dd685acb
Jason Yang <hao_yu_yang@outlook.com>
2017-01-12 19:12:06 +0800

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -1,11 +1,11 @@
-# count
-# noticed that sql can't use '!=', then use '<>' instead
+#count
+#noticed that sql can't use '!=', then use '<>' instead
 SELECT count(*) FROM databases.table where FID0001='1' and FID0002 <> '0';
-# the SELECT operation can't creat a new table
+#the SELECT operation can't creat a new table
 CREATE TABLE CID_to_CPI1
 SELECT `cpi1/dbid`.*,newfinaldrugnamecid.CID
 FROM `cpi1/dbid;
-# Left join
+#Left join
 CREATE TABLE CID_to_CPI1
 SELECT `cpi1/dbid`.*,newfinaldrugnamecid.CID
 FROM `cpi1/dbid`
